2f8b6cfebc968039478ad617d11e95da1c23c5a6
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-07-06 23:45:01 -0300

    teste rota get

--- src/server.js
+++ src/server.js
@@ -7,3 +7,19 @@ const app = express();
 app.use(cors());
 dotenv.config();
 app.use(express.json());
+
+
+//rota de teste
+
+app.get("/ola", (req, res)=>{
+    
+    res.status(222).send('FUNFOU')
+})
+
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log("servidor rodando")
+})
